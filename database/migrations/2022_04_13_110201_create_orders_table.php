f742ebddb8c5595de1a07e1dd31b714ea7803cba
Mohammed Naji <m.naji1512@gmail.com>
2022-04-13 14:25:35 +0300

    database

--- database/migrations/2022_04_13_110201_create_orders_table.php
+++ database/migrations/2022_04_13_110201_create_orders_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('type')->default('user');
-            $table->rememberToken();
+            $table->foreignId('user_id');
+            $table->float('total');
+            $table->float('specail_discount');
+            $table->string('discount_type');
+            $table->enum('status', ['completed', 'refund', 'canceld']);
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
